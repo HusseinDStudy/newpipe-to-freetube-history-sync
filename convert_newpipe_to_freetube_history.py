8a72c5d6702ea77ed2635826bb9a658289fbfca3
HusseinDStudy <85683989+HusseinDStudy@users.noreply.github.com>
2025-01-21 09:04:06 +0100

    Rename file to an explicit name

--- convert_newpipe_to_freetube_history.py
+++ convert_newpipe_to_freetube_history.py
@@ -1,8 +1,18 @@
 import sqlite3
 import os, json
+import datetime
 
 try:
-    conn = sqlite3.connect(os.getenv('NEWPIPE_HISTORY_DB'))        
+    print("modified from the original version on https://github.com/iamtalhaasghar/newpipe-to-freetube-history-sync version de 09/03/2023")
+    filename = next(iter([f for f in os.listdir('.') if f.startswith('NewPipeData-')]), None)
+    if filename is not None:
+        print(filename)
+        os.system(f'unzip {filename}')
+    # Get the current directory
+    current_dir = os.getcwd()
+    # Set the path to the newpipe.db file in the current directory
+    newpipe_db_path = os.path.join(current_dir, "newpipe.db")
+    conn = sqlite3.connect(newpipe_db_path)        
 except Exception as e:
     print(e) 
     exit()
@@ -46,6 +56,15 @@ for row in rows:
     except Exception as e:
         continue
 print('Bad rows:', len(rows)-len(newpipe_history))
-with open('newpipe_tofreetube.db', 'w') as f:
+
+date_creation = datetime.datetime.now().strftime('%Y-%m-%d')
+final_name = f'freetube-history-{date_creation}.db'
+with open(final_name, 'w') as f:
     for i in newpipe_history:
         f.write(json.dumps(i)+'\n')
+
+# Close the connection
+conn.close()
+
+os.remove('newpipe.db')
+os.remove('newpipe.settings')
